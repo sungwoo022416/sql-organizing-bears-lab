aca02ae356162fac72066829af0769f8a44dbf37
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-06-08 07:06:23 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1,'Mr.Chocolate', 27, 'M', 'Brown', "Phlegmatic", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2,'Rowdy',20, 'F', 'Black', 'Not easy', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3,'Tabitha', 15, 'F', 'Purple', 'calm', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4,'Sergeant Brown', 25, 'M', 'Green', 'Boring', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5,'Melissa', 24, 'F', 'Yellow', 'Chill', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6,'Grinch', 14, 'M', 'White', 'Angry', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7,'Wendy', 18, 'F', 'Red', 'Persuasive', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8,null, 20, 'M', 'Brown', 'Neutral', 0);
